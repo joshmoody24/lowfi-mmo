560da2c120fbc4b3f65cb3810999753fd4baa4dd
Josh Moody <joshmoody24@gmail.com>
2023-06-27 19:49:34 -0600

    various updates, including admin header/title override

--- lowfi_mmo/lowfi_mmo/urls.py
+++ lowfi_mmo/lowfi_mmo/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+admin.site.site_header = "Lowfi MMO Admin"
+admin.site.site_title = "Lowfi MMO Admin"
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('game.urls')),
